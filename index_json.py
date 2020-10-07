fc97af9950e31b3d2e2dbbc2ec556a35ab83b915
mpichini1 <marcello.pichini@gmail.com>
2020-10-07 12:13:20 +0200

    updating json

--- index_json.py
+++ index_json.py
@@ -8,11 +8,16 @@ cll = 'index_level_1000'
 
 df = query_mongo(db, cll)
 
+df['Time'] = [int(x) for x in df['Time']]
 
 df = df.rename(columns={'Time': 'time', 'Index Value': 'value'})
 
+df = df.loc[df['time'] > 1467331200]
+print(df)
 
 df = str(df.to_dict(orient="records"))
+
+
 
 value = 'json = '
 
